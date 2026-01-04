7e4c4c4f9adffbf6d2cba1c9096de26fce52c129
Danielli <d4nielli.dev@gmail.com>
2026-01-04 16:17:24 -0300

    added input of indexes and array size

--- cpp 2026/arrays/average.cpp
+++ cpp 2026/arrays/average.cpp
@@ -3,16 +3,24 @@
 // Code made in 04/01/2026. Starting the year's learning with arrays
 
 int main(){
-    //array declaration
-    const int array_size = 5;
-    int arr[array_size] = {1, 2, 3, 4, 5};
-
-    // loop to sum all indexes
-    int sum = 0;
-    for (int i = 0; i < array_size; i++){
-        std::cout << arr[i] << std::endl;
-        sum += arr[i];
+    //cout and cin (size)
+    std::cout << "\nDigit a determined amount of numbers to know their average\n";
+    int array_size;
+    while (true){
+        std::cout << "Amount of numbers: ";
+        std::cin >> array_size;
+        if(array_size <= 1){std::cout << "Enter a number above 1.\n"; continue;}
+        else {break;}
     }
+
+    // loop to get the indexes and sum them
+    int sum = 0;
+    int numbers[array_size]{};
+    for (int i = 0; i < array_size; i++){
+        std::cin >> numbers[i];
+        sum += numbers[i];
+    }
+
 
     // need to make sum a float because cpp interprets int/int = int.
     float average = (float)sum/array_size;
