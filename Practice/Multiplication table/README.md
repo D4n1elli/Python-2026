5f1ece10fca364e365148bdf8ac28052b999aaec
Danielli <d4nielli.dev@gmail.com>
2026-01-19 15:50:29 -0300

    Rename project folder and move repo root to 2026

---
+++ Practice/Multiplication table/README.md
@@ -0,0 +1,19 @@
+# Multiplication Table
+
+A Python program that asks the user for three inputs:
+- Index number (number to be calculated)
+- Start of the multiplication
+- End of the multiplication
+
+The program checks if the inputs are valid (start must be smaller than the end)
+- if yes, the program continues
+- otherwise, it repeats until such requirements are met
+
+With these, it prints the multiplication table of the index number, with the start and end determined by the user
+For an example, if you want to calculate the multiplication table of 5, starting from 4 and ending in 7, it will output:
+- 5 x 4 = 20
+- 5 x 5 = 25
+- 5 x 6 = 30
+- 5 x 7 = 35
+
+The code loops until the user wants to quit by typing 0 on the index number
---
+++ Practice/Multiplication table/multi_table.py
@@ -0,0 +1,33 @@
+# Input, validation and calculates the mult table
+# Returns true or false (true: code is repeated, false: program finishes)
+def run_mult_table():
+    #inputs
+    index = int(input("Index: "))
+    if index == 0:
+        return False
+    
+    start = int(input("Start: "))
+    end = int(input("End: "))
+
+    while start > end: # validation
+        print("Start must be smaller than end.")
+        start = int(input("Start: "))
+        end = int(input("End: "))
+
+    # calculating and printing the result
+    while start <= end:
+        print(f"{index} x {start} = {index*start}")
+        start += 1
+    return True
+
+
+
+def main(): # main code
+    print("\nDigit 3 numbers to calculate the multiplication table!")
+    print("Digit 0 on the index number to finish the program.\n")
+
+    again = run_mult_table() 
+    while again: #loops until return 0
+        again = run_mult_table()
+
+main()
